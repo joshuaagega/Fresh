e69ea8fd2882936c5553189907e3905307a3e87d
joshua <javanmega0@gmail.com>
2019-02-19 11:38:31 +0300

    deleting a repetition

--- js/script.js
+++ js/script.js
@@ -11,17 +11,3 @@ $(document).ready(function() {
     alert("This is an image.");
   });
 });
-
-$(document).ready(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
-
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-    alert("This is an image.");
-  });
-});
